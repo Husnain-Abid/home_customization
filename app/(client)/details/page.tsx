9e0b00c99f05373de9fee12f1af63ed2ea4bb9f4
Husnain-Abid <134037646+Husnain-Abid@users.noreply.github.com>
2025-12-18 23:05:07 +0500

    accordian update

--- app/(client)/details/page.tsx
+++ app/(client)/details/page.tsx
@@ -117,6 +117,7 @@ export default function DetailsPage() {
           <AccordionItem
             key={section.key}
             value={section.key}
+              id={section.key}
             className="border border-gray-200 rounded-lg px-6 mb-6 last:mb-0 last:!border-b   "
           >
             <AccordionTrigger className="text-lg font-semibold text-[#4A4C56]">
