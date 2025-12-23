eabad2555645bf4487468479407d23346a9c2c92
Husnain-Abid <134037646+Husnain-Abid@users.noreply.github.com>
2025-12-23 14:14:43 +0500

    update

--- components/Customizations/MiddleSection.tsx
+++ components/Customizations/MiddleSection.tsx
@@ -147,13 +147,13 @@ export default function MiddleSection() {
         if (hasInteriorFeatures && filteredInteriorData) {
 
             // 🔥 PRIORITY 1: AC = NO AND STAIRS = NO → interior_NoAC_NoStairs
-            if (
-                selectedFeatures.airConditioner === 'no' &&
-                selectedFeatures.stairs === 'no' &&
-                (filteredInteriorData.sections?.interior_NoAC_NoStairs?.gallery?.length ?? 0) > 0
-            ) {
-                return filteredInteriorData.sections.interior_NoAC_NoStairs.gallery;
-            }
+            // if (
+            //     selectedFeatures.airConditioner === 'no' &&
+            //     selectedFeatures.stairs === 'no' &&
+            //     (filteredInteriorData.sections?.interior_NoAC_NoStairs?.gallery?.length ?? 0) > 0
+            // ) {
+            //     return filteredInteriorData.sections.interior_NoAC_NoStairs.gallery;
+            // }
 
             // 🔹 PRIORITY 2: STAIRS = NO → interior_NoStairs
             // if (
